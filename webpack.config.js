0da479b27f20f206050b2d27f55c6b426541e8ed
qiujiali0229 <qiujiali0229@163.com>
2017-01-11 19:13:53 +0800

    project init

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry : __dirname + '/js/app.js',
+    output : {
+        path : __dirname + './build',
+        filename : 'bundle.js'
+    },
+    module:{
+        loaders: [
+            {
+                test: /\.js$/,
+                loader:"jsx-loader"
+            }
+        ]
+    }
+}
